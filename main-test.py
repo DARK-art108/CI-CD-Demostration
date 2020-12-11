dcff38503c74488c4c7e50f9bb842780baa56693
Ritesh Yadav <daydreamingguy941@gmail.com>
2020-12-11 23:20:44 +0530

    Added CircleCI

---
+++ main-test.py
@@ -0,0 +1,8 @@
+from main import Add
+
+def TestAdd():
+    assert Add(10,10) == 20
+    print("Add Function works correctly!! :)")
+
+if __name__ == "__main__":
+    TestAdd()
---
+++ main.py
@@ -0,0 +1,8 @@
+def Add(a, b):
+    return a + b
+
+def HelloPydata():
+    print("Hai Everyone from Ritesh")
+
+if __name__ == "__main__":
+    HelloPydata()
